3323b7d51a91b891734bd02a1bff6252a42493b6
Bonnie Anidobi <35204089+bonniejay@users.noreply.github.com>
2022-07-13 19:16:32 +0000

    Challenge - 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let result=0;
+    for(i=0; i<arrayOfNumbers.length; i++) {
+      result+=arrayOfNumbers[i];
+    }
+    return result;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+let arrayOfNumbers=[6, 8, 15, 22, 36, 49];
+console.log(sumOfNumbers(arrayOfNumbers));
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let num=0;
+    for(i=0; i<arrayOfNumbers.length; i++) {
+      if(arrayOfNumbers[i] % 2 ===0) {
+        num++;
+      }
+    }
+    return num;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+console.log(countEvenNumbers(arrayOfNumbers));
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
